9f84ec270c7a3da34e944cd9412ac5a27902f96a
QuanNH97 <quannh220597@gmail.com>
2020-08-13 10:54:39 +0700

    Initial Commit

---
+++ layout_ex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layout_ex
+//
+//  Created by Quan Nguyen on 8/13/20.
+//  Copyright © 2020 quannh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
